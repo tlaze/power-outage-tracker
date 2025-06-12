b9d85afafa7f13e27545517337a79d36bfe8ff1a
Tom <tomlazore@gmail.com>
2025-06-12 17:50:09 -0400

    Updated outage-list component. Shows outage reports and can mark an outage as resolved

--- power-outage-ui/src/app/pages/outage-list/outage-list.component.ts
+++ power-outage-ui/src/app/pages/outage-list/outage-list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OutageService } from '../../services/outage.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-outage-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HttpClientModule
+  ],
   templateUrl: './outage-list.component.html',
   styleUrl: './outage-list.component.scss'
 })
-export class OutageListComponent {
+export class OutageListComponent implements OnInit {
+  outages: any[] = [];
+  loading = true;
+  error = '';
 
+  constructor(private outageService: OutageService){}
+
+  ngOnInit(){
+    this.outageService.getAllOutages().subscribe({
+      next: (data: any)=> {
+        this.outages = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.error = 'Failed to load outages';
+        this.loading = false;
+      },
+    });
+  }
+
+  markResolved(id: number){
+    this.outageService.resolve(id).subscribe(() => {
+      this.outages = this.outages.map(o => 
+        o.id === id ? { ...o, resolved: true } : o
+      )
+    })
+  }
 }
